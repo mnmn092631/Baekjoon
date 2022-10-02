94a6f08877381ec05024165bb7c0189e9647fa6f
juhee kang <mnmn092631@gmail.com>
2022-10-02 18:03:15 +0900

    1330번

---
+++ 2. 조건문/1330번.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const [A, B] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map(Number);
+
+if (A === B) {
+  console.log("==");
+} else if (A > B) {
+  console.log(">");
+} else {
+  console.log("<");
+}
