8cb1883a6eaa1db08e9bd233730419e980d49841
juhee kang <mnmn092631@gmail.com>
2022-10-04 15:44:38 +0900

    8393번

---
+++ 3. 반복문/8393번.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+const n = Number(input);
+
+let answer = 0;
+
+for (let i = 1; i <= n; i++) {
+  answer += i;
+}
+
+console.log(answer);
