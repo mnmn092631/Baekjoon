c1a20d615523451be43f95ca090f77d77b9b1136
juhee kang <mnmn092631@gmail.com>
2022-10-04 15:12:58 +0900

    2739번

---
+++ 3. 반복문/2739번.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+const N = Number(input);
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${N} * ${i} = ${N * i}`);
+}
