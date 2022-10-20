7423f8196f6a1e4c9e8dbf6f61a08ddd4eb4486f
juhee kang <mnmn092631@gmail.com>
2022-10-20 18:22:19 +0900

    2869번

---
+++ 7. 기본 수학 1/2869번.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const [A, B, V] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(Math.ceil((V - B) / (A - B)));
