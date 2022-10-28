e30ad9d54c3af2744cf90413bd3705a3562cbe3f
juhee kang <mnmn092631@gmail.com>
2022-10-29 04:31:16 +0900

    1427번

---
+++ 10. 정렬/1427번.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("");
+
+input.sort((a, b) => b - a);
+
+console.log(input.join(""));
