f6f367aca5f0d3c455ca9f1f8407c6132ab74358
juhee kang <mnmn092631@gmail.com>
2022-11-01 14:00:04 +0900

    11651번

---
+++ 10. 정렬/11651번.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+let [N, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+arr = arr.map((item) => item.split(" ").map(Number));
+
+let answer = "";
+arr
+  .sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+    return a[1] - b[1];
+  })
+  .forEach((item) => (answer += `${item[0]} ${item[1]}\n`));
+
+console.log(answer);
