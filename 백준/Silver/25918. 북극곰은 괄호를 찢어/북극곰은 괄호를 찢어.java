f0255d81b50ccb41fff119bd8e99020275595fb0
juhee kang <mnmn092631@gmail.com>
2024-01-25 13:34:46 +0900

    [Silver I] Title: 북극곰은 괄호를 찢어, Time: 172 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Silver/25918. 북극곰은 괄호를 찢어/북극곰은 괄호를 찢어.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static int getDays(int n, String str) {
+		if (n % 2 != 0) return -1;
+
+		int maxDep = 1;
+		int top = 0;
+		char[] stack = new char[n];
+		
+		for (int i = 0; i < n; i++) {
+			stack[top] = str.charAt(i);
+			if (top > 0 && stack[top - 1] != stack[top]) {
+				if (top > maxDep) maxDep = top;
+				top--;
+				continue;
+			}
+			top++;
+		}
+
+		if (top == 0) return maxDep;
+		return -1;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+
+		System.out.print(getDays(n, str));
+	}
+
+}
