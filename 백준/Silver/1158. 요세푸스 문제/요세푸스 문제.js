d9aba35c014ceded8bf6e6d82ccda383e716f9a5
juhee kang <mnmn092631@gmail.com>
2024-01-25 14:09:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 428 ms, Memory: 11388 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -4,14 +4,14 @@ let [n, k] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Numb
 const queue = Array.from({ length: n }, (_, i) => i + 1);
 const answer = [];
 
+let idx = 0;
 let count = 1;
 while (queue.length !== 0) {
-	let shiftItem = queue.shift();
 	if (count === k) {
-		answer.push(shiftItem);
+		answer.push(queue.splice(idx, 1));
 		count = 1;
 	} else {
-		queue.push(shiftItem);
+		idx = (idx + 1) % queue.length;
 		count++;
 	}
 }
